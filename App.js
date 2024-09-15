7f9e46e5c53dcd3a95a404907d77a4e8f4523a9d
Roxanne Lopez <roxanneaggielopez@gmail.com>
2024-09-15 12:06:47 +0800

    Add UI for unfinished and finished tasks section

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
 
 export default function App() {
+  const tasks = [
+    { id: 1, text: "Make AppDev application", isChecked: false },
+    { id: 2, text: "Study Calculus slides", isChecked: false },
+    { id: 3, text: "Finish IAS module + activities", isChecked: true },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Kim Seokjin</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Unfinished Tasks</Text>
+
+        <View style={styles.taskList}>
+        {tasks
+            .filter((task) => !task.isChecked)  // Filter out checked tasks
+            .map((task) => (
+              <Task key={task.id} text={task.text} />
+            ))}
+        </View>
+      </View>
+
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Finished Tasks</Text>
+
+        <View style={styles.taskList}>
+        {tasks
+            .filter((task) => task.isChecked)  // Filter out checked tasks
+            .map((task) => (
+              <Task key={task.id} text={task.text} isChecked = {task.isChecked}/>
+            ))}
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,7 +44,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80
   },
+  sectionContainer: {
+    marginBottom: 40,
+    marginHorizontal: 30,
+    backgroundColor: '#fff',
+    padding: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  sectionTitle: {
+    fontSize: 26,
+  },
+  taskList: {
+    marginTop: 20
+  }
 });
---
+++ components/Task.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+
+const Task = (props) => {
+  return(
+    <View style={styles.taskContainer}>
+      <View style={styles.leftSection}>
+        <Image style={styles.checkBox} source={props.isChecked ? require('../assets/checked.png') : require('../assets/unchecked.png')} />
+        <Text style={[styles.taskText, props.isChecked && styles.checkedText]}>{props.text}</Text>
+      </View>
+      <Image style={styles.delete} source={require('../assets/x_sign.png')}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  taskContainer: {
+    marginBottom: 13,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  leftSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  checkBox:{
+    width: 20,
+    height: 20,
+    marginRight: 20,
+  },
+  taskText: {
+    fontSize: 18
+  },
+  checkedText: {
+    textDecorationLine: 'line-through',
+    color: '#656565'
+  },
+  delete: {
+    height: 15,
+    width: 15
+  },
+});
+
+export default Task;
